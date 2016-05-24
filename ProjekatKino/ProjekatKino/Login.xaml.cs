2e8721be1a5dd98facc576542f9a4d615e24f7de
nadjatimelord <nadjuska.tr@gmail.com>
2016-05-24 23:31:27 +0200

    dodala formu, ali nije jos dovrsena, moram naci boje

--- ProjekatKino/ProjekatKino/Login.xaml.cs
+++ ProjekatKino/ProjekatKino/Login.xaml.cs
@@ -35,13 +35,13 @@ namespace ProjekatKino
             var korisnickoIme = usernameBox.Text;
             var sifra = passwordBox.Password;
             var korisnik = DataSource.DataSourceMenuMD.ProvjeraKorisnika(korisnickoIme, sifra);
-            if (korisnik != null && korisnik.KorisnikId > 1 )
+            if (korisnik != null && korisnik.KorisnikId >=1)
             {
                 this.Frame.Navigate(typeof(OdabirFilma), korisnik);
             }
-            else if (korisnik != null && korisnik.KorisnikId == 1)
+            else if (korisnik != null && korisnik.KorisnikId ==2)
             {
-                this.Frame.Navigate(typeof(RegistrujNovogClana), korisnik);
+                this.Frame.Navigate(typeof(nakonodabira), korisnik);
             }
             else
             {
--- ProjekatKino/ProjekatKino/nakonodabira.xaml.cs
+++ ProjekatKino/ProjekatKino/nakonodabira.xaml.cs
@@ -13,29 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProjekatKino
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class OdabirFilma : Page
+    public sealed partial class nakonodabira : Page
     {
-        public OdabirFilma()
+        public nakonodabira()
         {
             this.InitializeComponent();
         }
 
-        private void chkStud_Checked(object sender, RoutedEventArgs e)
+        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
-            chkPen.IsChecked = false;
+
         }
 
-        private void chkPen_Checked(object sender, RoutedEventArgs e)
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
         {
-            chkStud.IsChecked = false;
         }
-       
     }
 }
