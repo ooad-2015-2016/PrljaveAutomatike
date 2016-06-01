d8544829bdb6d5ed00aa2cdab105837e63b17075
graba4 <amarbegovic86@gmail.com>
2016-06-01 16:51:29 +0200

    dodavanje rfid, gps, pocetak ispravljanja mvvm problema

---
+++ ProjekatKino/ProjekatKino/ViewModels/LoginViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjekatKino.DataSource;
+using ProjekatKino.Models;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using ProjekatKino.Helper;
+using ProjekatKino.ProjekatKino_XamlTypeInfo;
+using ProjekatKino;
+using System.Windows.Input;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace ProjekatKino.ViewModels
+{
+    class LoginViewModel
+    {
+        public Korisnik _korisnik { get; set; }
+        public string _username { get; set; }
+        public string _password { get; set; }
+        public ICommand LoginUposlenik { get; set; }
+        public INavigationService NavigationServis { get; set; }
+
+    }
+}
--- ProjekatKino/ProjekatKino/Views/GPSView.xaml.cs
+++ ProjekatKino/ProjekatKino/Views/GPSView.xaml.cs
@@ -7,24 +7,31 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Maps;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using ProjekatKino.ViewModels;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProjekatKino
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class GPSView : Page
     {
-        public MainPage()
+
+        public GPSView()
         {
             this.InitializeComponent();
+            mapa.Style = MapStyle.Aerial3DWithRoads;
+            mapa.ZoomLevel = 20;
+
+            this.DataContext = new GPSViewModel(mapa);
         }
     }
 }
+
